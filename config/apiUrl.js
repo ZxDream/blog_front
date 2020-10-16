11fe7d2695e6a5433990c44161c0817fe45335b9
赵喜[0027016517] <zhao.xi2@whalecloud.com>
2020-10-16 12:14:01 +0800

    接口参数

--- config/apiUrl.js
+++ config/apiUrl.js
@@ -1,10 +1,11 @@
-const ipUrl = 'http://127.0.0.1:7001/default/';
+// const ipUrl = 'http://127.0.0.1:7001/default/';
+const ipUrlProd = 'http://http://39.106.108.23:7001/default/';
 
 const servicePath = {
-  getArticleList: `${ipUrl}getArticleList`,  //首页接口
-  getArticleById: `${ipUrl}getArticleById/`,  //详细页接口
-  getTypeInfo: `${ipUrl}getTypeInfo`,  //文章类别接口
-  getListById: `${ipUrl}getListById/`,  //根据ID获取文章列表
+  getArticleList: `${ipUrl}getArticleList`, //首页接口
+  getArticleById: `${ipUrl}getArticleById/`, //详细页接口
+  getTypeInfo: `${ipUrl}getTypeInfo`, //文章类别接口
+  getListById: `${ipUrl}getListById/`, //根据ID获取文章列表
 };
 
 export default servicePath;
