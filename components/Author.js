d90fd9fc87b6c8a5cd58dde7b93d4773fba12979
赵喜[0027016517] <zhao.xi2@whalecloud.com>
2020-10-16 16:38:30 +0800

    测试

--- components/Author.js
+++ components/Author.js
@@ -12,7 +12,7 @@ const Author = () => {
         />
       </div>
       <div className="author-introduction">
-        光头程序员，专注于WEB和移动前端开发。要录1000集免费前端视频的傻X。此地维权无门，此时无能为力，此心随波逐流。
+        黑发程序员
         <Divider>社交账号</Divider>
         <Avatar size={28} icon="github" className="account" />
         <Avatar size={28} icon="qq" className="account" />
