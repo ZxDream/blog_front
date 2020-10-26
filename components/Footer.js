a6a1a05208685d5a1ae6ba5de70bc5dac0705025
赵喜[0027016517] <zhao.xi2@whalecloud.com>
2020-10-26 14:11:38 +0800

    新开窗口

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,10 @@ import '../static/style/components/footer.css';
 const Footer = () => (
   <div className="footer-div">
     <div>博客由 React+Node+Ant Desgin+mySql+nginx搭建 </div>
-    ICP证：<a href="https://beian.miit.gov.cn/">陕ICP备2020015903号</a>
+    ICP证：
+    <a href="https://beian.miit.gov.cn/" target="_blank">
+      陕ICP备2020015903号
+    </a>
   </div>
 );
 
